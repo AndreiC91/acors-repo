17e4df66b7f251798c1c0956bb5ecbfd2262ac89
Andrei Cracana <sest_ak47@yahoo.com>
2017-01-28 21:01:45 +0200

    start

---
+++ hr_pf/src/main/java/com/hr2017/homepage/controller/HomePageController.java
@@ -0,0 +1,24 @@
+package com.hr2017.homepage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomePageController {
+	String message = "Welcome to Spring MVC!";
+	 
+	@RequestMapping("/homepage")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("homepage");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+	
+}
